c6fadbd9cf4f2978bf51a7e9ac62406dcf6c2606
Adosh Singh <adoshbhanoo@gmail.com>
2024-03-04 18:26:15 +0530

    #01 AS: initialized the app with boilerplate code for both backend and frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class=" text-red-500">HelloWorld</div>
+</template>
